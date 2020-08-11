cf5b099ca6c63b5a3943bd55638d1b858caf467f
MatteoMit <matteo.mitrano@3mlinformatica.it>
2020-08-11 17:52:02 +0200

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from "@angular/core";
+import { country } from "./country";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = "Angular7 Template-driven Form";
+
+  countryList: country[] = [
+    new country("1", "India"),
+    new country("2", "USA"),
+    new country("3", "England")
+  ];
+
+  onSubmit(contactForm) {
+    console.log(contactForm.value);
+  }
 }
