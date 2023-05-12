dd873498bfa78af670ca1dca653813dc128aa789
MatteoMit <55322663+MatteoMit@users.noreply.github.com>
2023-05-12 14:11:45 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Contact} from './model/Contact';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: "my-app",
@@ -10,6 +12,7 @@ import { Contact} from './model/Contact';
 export class AppComponent implements OnInit {
   title = "Template-driven Form and validation in Angular";
 
+  contactForm: FormsModule;
   contact: Contact;
 
   ngOnInit() {
