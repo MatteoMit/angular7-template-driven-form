5968fb8325a70211217973e7c900610a28b9593b
MatteoMit <55322663+MatteoMit@users.noreply.github.com>
2023-05-24 15:46:47 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component } from "@angular/core";
-import { country } from "./country";
+import { Component } from '@angular/core';
+import { country } from './country';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = "Angular7 Template-driven Form";
+  title = 'Angular7 Template-driven Form';
 
   countryList: country[] = [
-    new country("1", "India"),
-    new country("2", "USA"),
-    new country("3", "England")
+    new country('1', 'Italia'),
+    new country('2', 'USA'),
+    new country('3', 'England'),
   ];
 
   onSubmit(contactForm) {
