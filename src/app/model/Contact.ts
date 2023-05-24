594d3c4ee19d364b664e79cd5594c7b7fe999a1e
MatteoMit <55322663+MatteoMit@users.noreply.github.com>
2023-05-24 15:40:40 +0200

    1 file modified

--- src/app/model/Contact.ts
+++ src/app/model/Contact.ts
@@ -4,4 +4,18 @@ export class Contact {
   gender: string;
   isToc: boolean;
   email: string;
+
+  constructor(
+    firstname: string,
+    lastname: string,
+    gender: string,
+    isToc: boolean,
+    email: string
+  ) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.gender = gender;
+    this.isToc = isToc;
+    this.email = email;
+  }
 }
