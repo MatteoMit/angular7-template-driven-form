11ab83553bb407c2aaf1ceae248920af7230db65
MatteoMit <55322663+MatteoMit@users.noreply.github.com>
2023-05-24 16:39:23 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.contact = {
-      firstname: "",
-      lastname: "",
+      firstname: "Matteo",
+      lastname: "mitrano",
       gender: "male",
       isToc: true,
       email: ""
