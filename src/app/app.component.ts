d543a8c29afbb54a1088e7f27813c73b594e37b0
MatteoMit <55322663+MatteoMit@users.noreply.github.com>
2023-05-25 12:50:13 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,23 @@
-import { Component, OnInit } from "@angular/core";
-import { Contact} from './model/Contact';
-
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './model/Contact';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent implements OnInit {
-  title = "Template-driven Form and validation in Angular";
+  title = 'Template-driven Form and validation in Angular';
 
   contact: Contact;
 
   ngOnInit() {
     this.contact = {
-      firstname: "Matteo",
-      lastname: "Mitrano",
-      gender: "male",
-      isToc: true,
-      email: ""
+      firstname: '',
+      lastname: '',
+      gender: 'male',
+      isToc: false,
+      email: '',
     };
   }
 
@@ -27,4 +25,3 @@ export class AppComponent implements OnInit {
     console.log(contactForm.value);
   }
 }
-
